b37f2bcd819aa8b016d9eda04f23e6f6da60108a
ios <ios@iosdemac-mini.local>
2018-04-28 15:17:19 +0800

    Initial Commit

---
+++ Imformation/Imformation.h
@@ -0,0 +1,19 @@
+//
+//  Imformation.h
+//  Imformation
+//
+//  Created by ios on 2018/4/28.
+//  Copyright © 2018年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Imformation.
+FOUNDATION_EXPORT double ImformationVersionNumber;
+
+//! Project version string for Imformation.
+FOUNDATION_EXPORT const unsigned char ImformationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Imformation/PublicHeader.h>
+
+
